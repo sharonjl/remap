205a181c5054b3cf738159743db713f6b9fbcf65
Sharon Lourduraj <sharon.lourduraj@gmail.com>
2016-01-26 17:24:07 -0500

    Initial commit

---
+++ remap.go
@@ -0,0 +1,37 @@
+package remap
+
+import (
+	"reflect"
+)
+
+func FieldMap(from interface{}, to interface{}) {
+	fromMap := make(map[string]reflect.Value)
+
+	tFrom := reflect.TypeOf(from).Elem()
+	vFrom := reflect.ValueOf(from).Elem()
+
+	for i := 0; i < tFrom.NumField(); i++ {
+		fld := tFrom.Field(i)
+		fromMap[fld.Name] = vFrom.Field(i)
+	}
+
+	tTo := reflect.TypeOf(to).Elem()
+	vTo := reflect.ValueOf(to).Elem()
+
+	for i := 0; i < tTo.NumField(); i++ {
+		field := vTo.Field(i)
+		fieldName := tTo.Field(i).Name
+
+		if val, ok := fromMap[fieldName]; ok {
+			if field.Kind() == val.Kind() {
+				field.Set(val)
+			} else if field.Kind() == reflect.Ptr {
+				tmp := reflect.New(field.Type().Elem())
+				tmp.Elem().Set(val)
+				field.Set(tmp)
+			} else if val.Kind() == reflect.Ptr {
+				field.Set(val.Elem())
+			}
+		}
+	}
+}
---
+++ remap_test.go
@@ -0,0 +1,62 @@
+package remap
+
+import (
+	"testing"
+	"time"
+)
+
+type StudentDetail struct {
+	Id          int
+	Name        string
+	Address     string
+	Grade       int
+	DOB         *time.Time
+	ModifiedOn  *time.Time
+	CreatedTime time.Time
+}
+
+type Student struct {
+	Name        string
+	Grade       int
+	DOB         *time.Time
+	ModifiedOn  time.Time
+	CreatedTime *time.Time
+}
+
+const twelveYears = time.Hour * 24 * 365 * 12
+
+var studentADob = time.Now()
+var studentACreated = studentADob.Add(twelveYears)
+var studentAModified = studentADob.Add(twelveYears).Add(time.Hour * 24)
+
+var studentA = &StudentDetail{
+	Id:          1,
+	Name:        "My string",
+	Address:     "12 My Street, My State, Country",
+	Grade:       int(6),
+	DOB:         &studentADob,
+	CreatedTime: studentACreated,
+	ModifiedOn:  &studentAModified,
+}
+
+func TestFieldMapping(t *testing.T) {
+	b := &Student{}
+
+	FieldMap(studentA, b)
+	if b.Name != studentA.Name ||
+		b.Grade != studentA.Grade {
+		t.Errorf("failed copying primitive values")
+	}
+
+	if b.DOB != studentA.DOB {
+		t.Errorf("failed copying ptr to ptr")
+	}
+
+	if b.CreatedTime.Sub(studentA.CreatedTime) != 0 {
+		t.Errorf("failed copying val to ptr")
+	}
+
+	if studentA.ModifiedOn.Sub(b.ModifiedOn) != 0 {
+		t.Errorf("failed copying ptr to val")
+	}
+}
